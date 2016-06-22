032b64cdedbabf758133669367cc388f0b4b9e85
HeZaiJin <1376277145@qq.com>
2016-06-22 16:30:26 +0800

    add java test lib ,change launcher icon

---
+++ javalibs/src/main/java/com/example/MyClass.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MyClass {
+
+    /**
+     * 100个猴子围成一个圈，从某一个猴子开始编号，它们的编号是1到100，从1号开始1,2,3,4,5,1,2,3,4,5循环报数，报到5的猴子就出列，直到所有猴子出列为止，请编程打印出猴子出列的编号顺序。
+     * @param args
+     */
+    public static void main(String[] args){
+        ArrayList<Integer> mMonkeys = new ArrayList<>();
+        System.out.println("Test");
+        // init
+        for (int i =1;i<=100;i++){
+            mMonkeys.add(i);
+        }
+        int offset = 0;
+        ArrayList<Integer> mValues = new ArrayList<>();
+        while (mValues.size() < 100){
+            Iterator<Integer> iterator = mMonkeys.iterator();
+            ArrayList<Integer> tmp = new ArrayList<>();
+            int j = offset;
+            while (iterator.hasNext()){
+                Integer next = iterator.next();
+//                int index = mMonkeys.indexOf(next) + 1  ;
+                j++;
+                if (j == 5 ){
+                    j =0;
+                    System.out.println("add mokey :"+next);
+                    tmp.add(next);
+                    mValues.add(next);
+                }
+                offset = j;
+            }
+            mMonkeys.removeAll(tmp);
+            System.out.println("last mokeys :"+mMonkeys.size());
+        }
+
+        for (int i=0;i<mValues.size();i++){
+            System.out.print(mValues.get(i)+",");
+        }
+
+    }
+}
