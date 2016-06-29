650bc264dead75f9f6f1538707d726a17c68f6b6
HeZaiJin <1376277145@qq.com>
2016-06-29 18:39:37 +0800

    slanted textview

---
+++ app/src/main/java/com/haozhang/gank/ui/TestActivity.java
@@ -0,0 +1,16 @@
+package com.haozhang.gank.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.haozhang.gank.R;
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+}
---
+++ widget/src/main/java/com/haozhang/widget/SlantedTextVeiw.java
@@ -0,0 +1,152 @@
+package com.haozhang.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.os.Build;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * SlantedTextView 倾斜的文字 不要设置背景
+ *
+ * @author HaoZhang
+ * @date 2016/6/29.
+ */
+public class SlantedTextVeiw extends View {
+
+    private Paint mPaint;
+    private TextPaint mTextPaint;
+    private float mSlantedLength = 40;
+    private float mTextSize = 16;
+    private int mSlantedBackgroundColor = Color.RED;
+    private int mTextColor = Color.WHITE;
+    private String mSlantedText = "";
+    private int mMode = 0;
+
+    public SlantedTextVeiw(Context context) {
+        this(context, null);
+    }
+
+    public SlantedTextVeiw(Context context, AttributeSet attrs) {
+        this(context, attrs, -1);
+    }
+
+    public SlantedTextVeiw(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SlantedTextVeiw(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    public void init(AttributeSet attrs) {
+        TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.SlantedTextVeiw);
+
+        mTextSize = array.getDimension(R.styleable.SlantedTextVeiw_slantedTextSize, mTextSize);
+        mTextColor = array.getColor(R.styleable.SlantedTextVeiw_slantedTextColor, mTextColor);
+        mSlantedLength = array.getDimension(R.styleable.SlantedTextVeiw_slantedLength, mSlantedLength);
+        mSlantedBackgroundColor = array.getColor(R.styleable.SlantedTextVeiw_slantedBackgroundColor, mSlantedBackgroundColor);
+        mSlantedText = array.getString(R.styleable.SlantedTextVeiw_slantedText);
+        if (array.hasValue(R.styleable.SlantedTextVeiw_slantedMode)) {
+            mMode = array.getInt(R.styleable.SlantedTextVeiw_slantedMode, 0);
+        }
+        array.recycle();
+
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(mSlantedBackgroundColor);
+
+        mTextPaint = new TextPaint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setTextSize(mTextSize);
+        mTextPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.ADD));
+        mTextPaint.setColor(mTextColor);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawBackground(canvas);
+        drawText(canvas);
+    }
+
+
+    public void drawBackground(Canvas canvas) {
+        Path path = new Path();
+        int w = getWidth();
+        int h = getHeight();
+        if (w != h) throw new IllegalStateException("SlantedTextView's width must equal to height");
+        if (mMode == 0) {
+            path.lineTo(w, h);
+            path.lineTo(w, h - mSlantedLength);
+            path.lineTo(mSlantedLength, 0);
+        }else {
+            path.moveTo(w,0);
+            path.lineTo(0, h);
+            path.lineTo(0,h - mSlantedLength);
+            path.lineTo(w-mSlantedLength, 0);
+        }
+        path.close();
+        canvas.drawPath(path, mPaint);
+
+        canvas.save();
+    }
+
+    public void drawText(Canvas canvas) {
+        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+        float fontHeight = fontMetrics.bottom - fontMetrics.top;
+        float textBaseY = getHeight() - (getHeight() - fontHeight) / 2 - fontMetrics.bottom;
+        double offset = mSlantedLength / 4 - 1;
+        float toY = (float) (textBaseY - offset);
+        int angle =  45;
+        if (mMode == 1){
+            angle = -45;
+            offset = -offset;
+        }
+        float toX = (float) (getWidth() / 2 + offset);
+        canvas.rotate(angle, toX, toY);
+        canvas.drawText(mSlantedText, toX, toY, mTextPaint);
+    }
+
+    public void setText(String str) {
+        mSlantedText = str;
+        postInvalidate();
+    }
+
+    public void setText(int res) {
+        String str = getResources().getString(res);
+        if (!TextUtils.isEmpty(str)) {
+            setText(str);
+        }
+    }
+
+    public String getText() {
+        return mSlantedText;
+    }
+
+    public void setSlantedBackgroundColor(int color) {
+        mSlantedBackgroundColor = color;
+        postInvalidate();
+    }
+
+    public void setTextColor(int color) {
+        mTextColor = color;
+        postInvalidate();
+    }
+
+}
